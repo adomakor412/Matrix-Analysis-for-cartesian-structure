1f8000a83a343f8d3356bcbf42d747bb22b2a0ec
adomakor412 <ronaldadomako@icloud.com>
2018-12-29 21:29:10 -0500

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -2,16 +2,16 @@ class Matrix: #matrices are lists; scalers are floats
     def __init__(self):
         return
     def addMatrices(self, matrix1, matrix2):
-        myMatrix = [
+        return [
             [matrix1[m][n] + matrix2[m][n] for n in range(len(matrix1[m]))]
             for m in range(len(matrix1))
         ]
-        return myMatrix
     def subtractMatrices(self, matrix1, matrix2):
-        return
+        myMatrix = addMatrices(matrix1, scaleMatrix(matrix2, float(-1))
+        return myMatrix
     def multiplyMatrices(self, matrix1, matrix2):
         return
-    def multiplyMatrix(self, matrix1, scaler):
+    def scaleMatrix(self, matrix1, scaler):
         return
     def inverse(self, matrix1):
         return
